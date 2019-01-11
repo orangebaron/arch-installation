8a6517f66486cf65c2ea6227fcad2c1910d99ba5
Sam Alws <saalws@ctemc.org>
2019-01-10 23:13:31 -0500

    dwm to work better on laptop

--- homeuwepurgatorydwmconfig.h
+++ homeuwepurgatorydwmconfig.h
@@ -27,10 +27,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "discord",  NULL,       NULL,       1 << 0,       0,           1 },
-	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           1 },
+	{ "discord",  NULL,       NULL,       1 << 1,       0,           1 },
+	{ "Firefox",  NULL,       NULL,       1 << 2,       0,           1 },
 	{ "ckb-next", NULL,       NULL,       1 << 8,       0,           1 },
 	{ "VirtualBox Manager", NULL, NULL,   1 << 7,       0,           1 },
+	{ "VirtualBox Machine", NULL, NULL,   1 << 3,       0,           1 },
 };
 
 /* layout(s) */
